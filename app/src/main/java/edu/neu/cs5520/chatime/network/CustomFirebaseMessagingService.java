f2d81759a29f2237200bca8d0ac86773cd2c0291
zacw7 <zac.wen7@gmail.com>
2021-03-29 21:17:52 -0700

    Handle notification when found someone to chat with

---
+++ app/src/main/java/edu/neu/cs5520/chatime/network/CustomFirebaseMessagingService.java
@@ -0,0 +1,31 @@
+package edu.neu.cs5520.chatime.network;
+
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class CustomFirebaseMessagingService extends FirebaseMessagingService {
+
+  public CustomFirebaseMessagingService() {
+    super();
+  }
+
+  @Override
+  public void onMessageReceived(RemoteMessage remoteMessage) {
+    super.onMessageReceived(remoteMessage);
+    Log.d("TAG", "From: " + remoteMessage.getFrom());
+
+    if (!remoteMessage.getData().isEmpty()) {
+      Log.d("TAG", "Received Data: " + remoteMessage.getData().toString());
+      String roomId = remoteMessage.getData().get("roomId");
+      Intent intent = new Intent("room-id-event");
+      // You can also include some extra data.
+      intent.putExtra("roomId", roomId);
+      LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+  }
+}
