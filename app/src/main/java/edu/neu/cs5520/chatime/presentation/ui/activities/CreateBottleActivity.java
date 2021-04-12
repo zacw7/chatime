df26ed74ec9aa47dd07412c56995b0edeb4b84ac
zacw7 <zac.wen7@gmail.com>
2021-04-12 14:18:11 -0700

    Add colors to icons

--- app/src/main/java/edu/neu/cs5520/chatime/presentation/ui/activities/CreateBottleActivity.java
+++ app/src/main/java/edu/neu/cs5520/chatime/presentation/ui/activities/CreateBottleActivity.java
@@ -1,5 +1,7 @@
 package edu.neu.cs5520.chatime.presentation.ui.activities;
 
+import static java.security.AccessController.getContext;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
@@ -13,9 +15,11 @@ import android.widget.RadioButton;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,12 +51,21 @@ public class CreateBottleActivity extends AppCompatActivity implements CreateBot
     RadioButton mRadioFromCamera;
     @BindView(R.id.radio_from_gallery)
     RadioButton mRadioFromGallery;
+    @BindView(R.id.switch_multiple_receivers)
+    SwitchMaterial mSwitchMultipleReceivers;
+    @BindView(R.id.image_add_audio)
+    ImageView mImageAddAudio;
+    @BindView(R.id.image_add_photo)
+    ImageView mImageAddPhoto;
+    @BindView(R.id.image_send_to)
+    ImageView mImageSendTo;
+    @BindView(R.id.image_multiple_receivers)
+    ImageView mImageMultipleReceivers;
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_PICK_IMAGE = 2;
     private CreateBottlePresenter mPresenter;
     private String mCurrentPhotoPath;
-    private boolean mAddPhotoEnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,6 +111,7 @@ public class CreateBottleActivity extends AppCompatActivity implements CreateBot
     @Override
     public void displayAddedPhoto(Uri uri) {
         mLayoutAddPhoto.setVisibility(View.VISIBLE);
+        mImageAddPhoto.setColorFilter(getResources().getColor(R.color.blue));
         Glide.with(this).load(uri).into(mImageBottlePhoto);
         mRadioFromCamera.setEnabled(false);
         mRadioFromGallery.setEnabled(false);
@@ -106,6 +120,7 @@ public class CreateBottleActivity extends AppCompatActivity implements CreateBot
     @Override
     public void hidePhotoPreview() {
         mLayoutAddPhoto.setVisibility(View.GONE);
+        mImageAddPhoto.clearColorFilter();
         mImageBottlePhoto.setImageResource(android.R.color.transparent);
         mRadioFromCamera.setEnabled(true);
         mRadioFromGallery.setEnabled(true);
@@ -113,7 +128,15 @@ public class CreateBottleActivity extends AppCompatActivity implements CreateBot
 
     @OnClick(R.id.layout_add_audio)
     public void recordAudio() {
-        System.out.println("audio audio!!");
+    }
+
+    @OnClick(R.id.switch_multiple_receivers)
+    public void onMultipleReceiversClick() {
+        if (mSwitchMultipleReceivers.isChecked()) {
+            mImageMultipleReceivers.setColorFilter(getResources().getColor(R.color.orange));
+        } else {
+            mImageMultipleReceivers.clearColorFilter();
+        }
     }
 
     @Override
